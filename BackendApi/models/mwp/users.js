efb5beda228ac4c14dea926f0d0974f657792da2
Smriti <smriti12sci.nvm@gmail.com>
2024-12-20 15:24:51 +0530

    phone number datatype change to string

--- BackendApi/models/mwp/users.js
+++ BackendApi/models/mwp/users.js
@@ -142,7 +142,7 @@ module.exports = (sequelize, DataTypes) => {
         notNull: true,
       },
       phone: {
-          type: DataTypes.INTEGER,
+          type: DataTypes.STRING,
           allowNull: false,
           unique: true,
           validate: {
